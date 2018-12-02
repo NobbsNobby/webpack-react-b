7c404532bae0e885703abfe299934ea2614949d2
NobbsNobby <art.kotov1@gmail.com>
2018-12-02 13:16:20 +0300

    2 space to 4

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -2,15 +2,15 @@ import React, { Component } from 'react';
 import Styles from './styles.m.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className={Styles.app}>
-        <h1 className={Styles.header}>
-          Everything is ready to start
-        </h1>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className={Styles.app}>
+                <h1 className={Styles.header}>
+                  Everything is ready to start
+                </h1>
+            </div>
+        );
+    }
 }
 
 export default App;
