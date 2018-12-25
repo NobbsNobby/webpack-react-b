5b9f66fd79937799746c039281e264360080ab14
NobbsNobby <art.kotov1@gmail.com>
2018-12-26 00:30:57 +0300

    add eslint rules - part 3

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,11 +1,12 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import Styles from './styles.m.css';
 
 class App extends Component {
     render() {
         return (
-            <div className={Styles.app}>
-                <h1 className={Styles.header}>Everything is ready to start</h1>
+            <div className = {Styles.app}>
+                <Button />
+                <h1 className = {Styles.header}>Everything is ready to start</h1>
             </div>
 
         );
