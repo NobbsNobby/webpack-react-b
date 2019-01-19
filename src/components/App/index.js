5edfb959bdfb3268983ea231011b5d1b062d8584
NobbsNobby <art.kotov1@gmail.com>
2019-01-19 21:07:25 +0300

    update package

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,14 +1,14 @@
-import React, { Component } from 'react';
+import * as React from 'react';
 import Styles from './styles.m.css';
 
-class App extends Component {
+class App extends React.Component {
+  
     render() {
         return (
-            <div className = {Styles.app}>
-                <Button />
-                <h1 className = {Styles.header}>Everything is ready to start</h1>
+            <div className = { Styles.app }>
+                <h1 className = { Styles.header }>Everything is ready to start</h1>
             </div>
-
+    
         );
     }
 }
